acdcca961234a33b4f1342ae883756c30a39ab53
diegookawa <diego.arenas1505@gmail.com>
2024-08-27 20:37:37 -0300

    Coordinates definition page

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv, set_key
 import os
 
@@ -8,13 +8,18 @@ load_dotenv()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    api_key = None
     if request.method == "POST":
         api_key = request.form.get("apiKey")
         if api_key:
             dotenv_path = '../.env'
             set_key(dotenv_path, 'KEY', api_key)
-    return render_template("main_page.html", api_key=api_key)
+            return redirect(url_for('coordinates_definition'))
+    return render_template("main_page.html")
+
+@app.route("/coordinates_definition")
+def coordinates_definition():
+    api_key = os.getenv('KEY')
+    return render_template("coordinates_definition.html", api_key=api_key)
 
 if __name__ == "__main__":
     app.run()
